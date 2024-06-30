0669affba663f70aff7534ae7513476b5e8745f3
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2024-06-30 23:13:41 +0200

    Added docstrings for write-tree

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,7 +1,15 @@
 use crate::objects::tree::Tree;
-use anyhow::Context;
 use std::path::Path;
 
+/// Writes a tree object from the given path to the database, and prints the resulting object ID.
+///
+/// # Arguments
+///
+/// * `path` - The path to the directory to write the tree object from.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the resulting object ID, or an `anyhow::Error` if an error occurs.
 pub(crate) fn invoke(path: &Path) -> anyhow::Result<()> {
     let oid = Tree::write_for_dir(path)?.context("Failed to write tree.")?;
     println!("{}", oid.hash());
